1529856b4a79e503cdf378dbb76acd7144ee830d
Mahendra Gohil <mahendra.gohil@intigral.net>
2025-04-05 19:35:18 +0530

    add Toast for no internet scenario

--- App.tsx
+++ App.tsx
@@ -5,11 +5,37 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import RootStack from './src/navigation/RootStack';
+import Toast from 'react-native-toast-message';
+import {useNetInfo} from '@react-native-community/netinfo';
 
 function App() {
-  return <RootStack />;
+  const netInfo = useNetInfo();
+  const hasShownToast = useRef(false);
+
+  useEffect(() => {
+    if (netInfo.isConnected === false && !hasShownToast.current) {
+      Toast.show({
+        type: 'error',
+        text1: 'No Internet',
+        text2: 'Please check your connection.',
+        position: 'bottom',
+        visibilityTime: 3000,
+      });
+      hasShownToast.current = true;
+    }
+
+    if (netInfo.isConnected === true) {
+      hasShownToast.current = false;
+    }
+  }, [netInfo.isConnected]);
+  return (
+    <>
+      <RootStack />
+      <Toast />
+    </>
+  );
 }
 
 export default App;
