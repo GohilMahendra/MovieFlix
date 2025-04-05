9611e7ae2615f520d830041c697ad4e588786874
Mahendra Gohil <mahendra.gohil@intigral.net>
2025-04-05 19:06:24 +0530

    re write unit test for search changes

--- __tests__/hooks/search/useSearch.steps.tsx
+++ __tests__/hooks/search/useSearch.steps.tsx
@@ -1,27 +1,26 @@
-import { renderHook } from "@testing-library/react-hooks"
-import { act, render, waitFor } from "@testing-library/react-native"
-import useSearch from "../../../src/hooks/search/useSearch"
+import {renderHook} from '@testing-library/react-hooks';
+import {act, render, waitFor} from '@testing-library/react-native';
+import useSearch from '../../../src/hooks/search/useSearch';
 
-jest.mock("../../../src/apis/MovieApi",()=>{
-    const { apiSerachResultResponse } = require("../../mocks/MockData")
-    return{
-        searchMovies: jest.fn().mockResolvedValue(apiSerachResultResponse)
-    }
-})
-jest.useFakeTimers()
-jest.runAllTimers()
+jest.mock('../../../src/apis/MovieApi', () => {
+  const {apiSerachResultResponse} = require('../../mocks/MockData');
+  return {
+    searchMovies: jest.fn().mockResolvedValue(apiSerachResultResponse),
+  };
+});
+jest.useFakeTimers();
+jest.runAllTimers();
 
-describe("useSearch hooks ...",()=>{
-    it("search will be called on change of search term",async()=>{
-        const { result } = renderHook(()=>useSearch())
-    
-        await act(async()=>{
-            result.current.setSearchTerm("die")
-        })
-        expect(result.current.searchTerm).toBe("die")
-        jest.advanceTimersByTime(2000)
-        await waitFor(async()=>{
-            expect(result.current.movies.length).toBe(20)
-        })
-    })
-})
+describe('useSearch hooks ...', () => {
+  it('search will be called on change of search term', async () => {
+    const {result} = renderHook(() => useSearch());
+    await act(async () => {
+      result.current.setSearchTerm('dieH');
+    });
+    expect(result.current.searchTerm).toBe('dieH');
+    jest.advanceTimersByTime(2000);
+    await waitFor(async () => {
+      expect(result.current.movies.length).toBe(20);
+    });
+  });
+});
--- __tests__/screens/search/Search.steps.tsx
+++ __tests__/screens/search/Search.steps.tsx
@@ -1,68 +1,71 @@
-import { act, fireEvent, render, screen, waitFor } from "@testing-library/react-native"
-import Search from "../../../src/screens/search/Search"
-import MovieCard from "../../../src/components/movies/MovieCard";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
+import Search from '../../../src/screens/search/Search';
+import MovieCard from '../../../src/components/movies/MovieCard';
 
-const mockNavigate = jest.fn()
-const mockGoBack = jest.fn()
+const mockNavigate = jest.fn();
+const mockGoBack = jest.fn();
 jest.mock('@react-navigation/native', () => ({
-    ...jest.requireActual('@react-navigation/native'),
-    useNavigation: () => ({
-        navigate: mockNavigate,
-        goBack: mockGoBack
-    }),
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: () => ({
+    navigate: mockNavigate,
+    goBack: mockGoBack,
+  }),
 }));
 
-jest.mock("../../../src/apis/MovieApi", () => {
-    const { apiSerachResultResponse } = require("../../mocks/MockData")
-    return {
-        searchMovies: jest.fn().mockResolvedValue(apiSerachResultResponse)
-    }
-})
+jest.mock('../../../src/apis/MovieApi', () => {
+  const {apiSerachResultResponse} = require('../../mocks/MockData');
+  return {
+    searchMovies: jest.fn().mockResolvedValue(apiSerachResultResponse),
+  };
+});
 
-jest.useFakeTimers()
+jest.useFakeTimers();
 
-describe("search screen ...",()=>{
-    beforeEach(()=>{
-        render(
-            <Search/>
-        )
-    })
+describe('search screen ...', () => {
+  beforeEach(() => {
+    render(<Search />);
+  });
 
-    it("i can search the word in input",async()=>{
-        const input = screen.getByTestId("input_search")
-        jest.runAllTimers()
-        await act(async()=>{
-            fireEvent(input,"changeText","og")
-        })
-        jest.advanceTimersByTime(1000)
-        const list = screen.getByTestId("list_search")
-          await waitFor(async()=>{
-            expect(list.props.data.length).toBe(20)
-        })
-    })
+  it('i can search the word in input', async () => {
+    const input = screen.getByTestId('input_search');
+    jest.runAllTimers();
+    await act(async () => {
+      fireEvent(input, 'changeText', 'pushpa');
+    });
+    jest.advanceTimersByTime(1000);
+    const list = screen.getByTestId('list_search');
+    await waitFor(async () => {
+      expect(list.props.data.length).toBe(20);
+    });
+  });
 
-    it("I can press on the result will call navigate to other screen",async()=>{
+  it('I can press on the result will call navigate to other screen', async () => {
+    const input = screen.getByTestId('input_search');
+    jest.runAllTimers();
+    await act(async () => {
+      fireEvent(input, 'changeText', 'pushpa 2');
+    });
+    jest.advanceTimersByTime(500);
+    await waitFor(async () => {
+      const allCards = screen.root.findAllByType(MovieCard);
 
-        const input = screen.getByTestId("input_search")
-        jest.runAllTimers()
-        await act(async()=>{
-            fireEvent(input,"changeText","og")
-        })
-        jest.advanceTimersByTime(500)
-        await waitFor(async()=>{
-            const allCards = screen.root.findAllByType(MovieCard);
+      const card = allCards[0];
+      await act(async () => {
+        card.props.onMoviePress(100);
+      });
+      expect(mockNavigate).toHaveBeenCalled();
+    });
+  });
 
-            const card = allCards[0]
-            await act(async()=>{
-                card.props.onMoviePress(100)
-            })
-            expect(mockNavigate).toHaveBeenCalled()
-        })
-    })
-
-    it("I can go back by pressing back button",()=>{
-        const back_btn = screen.getByTestId("btn_goBack")
-        fireEvent(back_btn,"press")
-        expect(mockGoBack).toHaveBeenCalled()
-    })
-})
+  it('I can go back by pressing back button', () => {
+    const back_btn = screen.getByTestId('btn_goBack');
+    fireEvent(back_btn, 'press');
+    expect(mockGoBack).toHaveBeenCalled();
+  });
+});
